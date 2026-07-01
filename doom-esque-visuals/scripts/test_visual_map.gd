d01b5fc0fa027d1f2f79f2a23a72bc3718163598
egg <134161546+The-HandyDandyman@users.noreply.github.com>
2026-06-30 23:08:07 -0600

    Added multiple environments
    AAAAAAA

--- doom-esque-visuals/scripts/test_visual_map.gd
+++ doom-esque-visuals/scripts/test_visual_map.gd
@@ -3,10 +3,24 @@ extends Node3D
 @onready var Text:Control = $Text/RichTextLabel
 @onready var WorldEnv:WorldEnvironment = $WorldEnvironment
 
+@export var environment_a:Environment
+@export var environment_b:Environment
+@onready var skybox:MeshInstance3D = $FullMap/LazySkyboxBecauseThisIsATestMap
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		WorldEnv.environment.fog_enabled = not WorldEnv.environment.fog_enabled
 	if Input.is_action_just_pressed("ui_text_backspace"):
 		Text.visible = not Text.visible
-	Text.text = "Q and E to adjust camera\nA and D to rotate Godot plush\n" + ScreenOverlay.paletteRelatedText + "Escape to toggle fog (Currently set to " + str(WorldEnv.environment.fog_enabled) + ")\nBackspace to hide instructions"
+	if Input.is_action_just_pressed("ui_text_indent"):
+		if environment_a == null or environment_b == null: return
+		skybox.visible = not skybox.visible
+		var newEnvironment:Environment
+		if skybox.visible:
+			newEnvironment = environment_a
+		else:
+			newEnvironment = environment_b
+		newEnvironment.fog_enabled = WorldEnv.environment.fog_enabled
+		WorldEnv.environment = newEnvironment
+	Text.text = "Q and E to adjust camera\nA and D to rotate Godot plush\n" + ScreenOverlay.paletteRelatedText + "Escape to toggle fog (Currently set to " + str(WorldEnv.environment.fog_enabled) + ")\nTab to toggle environment\nBackspace to hide instructions"
