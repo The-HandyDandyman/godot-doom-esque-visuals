edb8da5c974b5f2701b2e875d4b8028b8e2adbd0
egg <134161546+The-HandyDandyman@users.noreply.github.com>
2026-07-31 00:17:49 -0600

    Light masks, lightmaps (mostly) correctly calculated in sprites
    AAAAAAAA

--- doom-esque-visuals/scripts/directional_object_preview.gd
+++ doom-esque-visuals/scripts/directional_object_preview.gd
@@ -2,6 +2,7 @@ extends MeshInstance3D
 
 @onready var animator:AnimationPlayer = $AnimationPlayer
 var directionAmount:float = 0.1
+var rotateToggle:bool = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animator.play("animate")
@@ -11,5 +12,11 @@ func _process(delta):
 		directionAmount += 0.01
 	elif Input.is_action_pressed("rotate_left"):
 		directionAmount -= 0.01
+	if Input.is_action_just_pressed("godotplush_rotate_toggle"):
+		rotateToggle = not rotateToggle
 	directionAmount = clamp(-0.4,0.4,directionAmount)
-	self.rotate_y(directionAmount)
+	if rotateToggle:
+		animator.speed_scale = 1
+		self.rotate_y(directionAmount)
+	else:
+		animator.speed_scale = 0
--- doom-esque-visuals/scripts/test_visual_map.gd
+++ doom-esque-visuals/scripts/test_visual_map.gd
@@ -40,4 +40,4 @@ func _process(delta):
 			cameraPoint.play("otherpoint")
 		else:
 			cameraPoint.play("RESET")
-	Text.text = "Q and E to adjust camera\nA and D to rotate Godot plush\n" + ScreenOverlay.paletteRelatedText + "Escape to toggle fog (Currently set to " + str(WorldEnv.environment.fog_enabled) + ")\nTab to toggle environment\nDel to switch view\nR and T to adjust ghost opacity\nH to switch ghost dither texture\nM to mute music\nBackspace to hide instructions"
+	Text.text = "Q and E to adjust camera\nA and D to rotate Godot plush\nW to toggle Godot plushie rotation\n" + ScreenOverlay.paletteRelatedText + "Escape to toggle fog (Currently set to " + str(WorldEnv.environment.fog_enabled) + ")\nTab to toggle environment\nDel to switch view\nR and T to adjust ghost opacity\nH to switch ghost dither texture\nM to mute music\nBackspace to hide instructions"
