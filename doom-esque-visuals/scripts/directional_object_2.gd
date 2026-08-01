d225f3760fe67a283cc7ab5c92f9e1be21282dcb
egg <134161546+The-HandyDandyman@users.noreply.github.com>
2026-08-01 10:42:46 -0600

    Adjusted color band calculation, added a third Godot Plush thingamabob to show lighting transition
    AAAAAAAAAAAAAAA

--- doom-esque-visuals/scripts/directional_object_2.gd
+++ doom-esque-visuals/scripts/directional_object_2.gd
@@ -3,11 +3,16 @@ extends MeshInstance3D
 
 @onready var animator:AnimationPlayer = $AnimationPlayer
 @onready var path:AnimationPlayer = $notAPath3DBecauseImLazy
+@export var multi_step:bool = false
 var directionAmount:float = 0.1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	material_override = material_override.duplicate() # Just doing this so that way the materials aren't duplicated in editor
-	move_time()
+	if multi_step:
+		move_time()
+	else:
+		animator.play("animate")
+		path.play("waddle")
 
 func move_time():
 	if animator.current_animation != "animate":
